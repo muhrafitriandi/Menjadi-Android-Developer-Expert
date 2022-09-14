ded482b091ae099ff07d6482d74fb40918b735e6
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-09-15 06:09:29 +0700

    create database module with dagger

--- Dependency Injection/TourismAppDagger/app/src/main/java/com/dicoding/tourismappdagger/core/data/source/local/room/TourismDatabase.kt
+++ Dependency Injection/TourismAppDagger/app/src/main/java/com/dicoding/tourismappdagger/core/data/source/local/room/TourismDatabase.kt
@@ -1,9 +1,6 @@
 package com.dicoding.tourismappdagger.core.data.source.local.room
 
-import android.content.Context
-
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 import com.dicoding.tourismappdagger.core.data.source.local.entity.TourismEntity
@@ -12,22 +9,4 @@ import com.dicoding.tourismappdagger.core.data.source.local.entity.TourismEntity
 abstract class TourismDatabase : RoomDatabase() {
 
     abstract fun tourismDao(): TourismDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: TourismDatabase? = null
-
-        fun getInstance(context: Context): TourismDatabase =
-            INSTANCE ?: synchronized(this) {
-            val instance = Room.databaseBuilder(
-                context.applicationContext,
-                TourismDatabase::class.java,
-                "Tourism.db"
-            )
-                .fallbackToDestructiveMigration()
-                .build()
-            INSTANCE = instance
-            instance
-        }
-    }
 }
