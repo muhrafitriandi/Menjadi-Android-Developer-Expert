37a876a4f67092d320d3e98b32bd4eeb06eed927
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-09-15 07:04:59 +0700

    viewmodel module configuration
    - mengubah kode pada ViewModelFactory agar bisa menerima ViewModel secara generic/umum.
    - membuat custom MapKey untuk membaca ViewModel.
    - membuat Module khusus untuk ViewModel.

--- Dependency Injection/TourismAppDaggerMultibindings/app/src/main/java/com/dicoding/tourismappdaggermultibindings/core/ui/ViewModelFactory.kt
+++ Dependency Injection/TourismAppDaggerMultibindings/app/src/main/java/com/dicoding/tourismappdaggermultibindings/core/ui/ViewModelFactory.kt
@@ -8,23 +8,17 @@ import com.dicoding.tourismappdaggermultibindings.di.AppScope
 import com.dicoding.tourismappdaggermultibindings.favorite.FavoriteViewModel
 import com.dicoding.tourismappdaggermultibindings.home.HomeViewModel
 import javax.inject.Inject
+import javax.inject.Provider
 
 @AppScope
-class ViewModelFactory @Inject constructor(private val tourismUseCase: TourismUseCase) :
-    ViewModelProvider.NewInstanceFactory() {
+class ViewModelFactory @Inject constructor(private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>) :
+    ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T =
-        when {
-            modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
-                HomeViewModel(tourismUseCase) as T
-            }
-            modelClass.isAssignableFrom(FavoriteViewModel::class.java) -> {
-                FavoriteViewModel(tourismUseCase) as T
-            }
-            modelClass.isAssignableFrom(DetailTourismViewModel::class.java) -> {
-                DetailTourismViewModel(tourismUseCase) as T
-            }
-            else -> throw Throwable("Unknown ViewModel class: " + modelClass.name)
-        }
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val creator = creators[modelClass] ?: creators.entries.firstOrNull {
+            modelClass.isAssignableFrom(it.key)
+        }?.value ?: throw IllegalArgumentException("unknown model class $modelClass")
+        return creator.get() as T
+    }
 }
