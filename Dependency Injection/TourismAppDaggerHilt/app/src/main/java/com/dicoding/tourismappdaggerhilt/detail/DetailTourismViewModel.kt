68181ab8280db8f872ae5ddabd63b048ea85ea61
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-09-15 07:36:53 +0700

    add annotation hiltviewmodel on each viewmodel

--- Dependency Injection/TourismAppDaggerHilt/app/src/main/java/com/dicoding/tourismappdaggerhilt/detail/DetailTourismViewModel.kt
+++ Dependency Injection/TourismAppDaggerHilt/app/src/main/java/com/dicoding/tourismappdaggerhilt/detail/DetailTourismViewModel.kt
@@ -3,8 +3,10 @@ package com.dicoding.tourismappdaggerhilt.detail
 import androidx.lifecycle.ViewModel
 import com.dicoding.tourismappdaggerhilt.core.domain.model.Tourism
 import com.dicoding.tourismappdaggerhilt.core.domain.usecase.TourismUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class DetailTourismViewModel @Inject constructor(private val tourismUseCase: TourismUseCase) : ViewModel() {
     fun setFavoriteTourism(tourism: Tourism, newStatus:Boolean) = tourismUseCase.setFavoriteTourism(tourism, newStatus)
 }
