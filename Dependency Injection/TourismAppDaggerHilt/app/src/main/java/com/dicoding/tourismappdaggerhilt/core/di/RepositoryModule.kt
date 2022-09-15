24938ab1ab1ec9d593699eb9828cfd7a839bc24b
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-09-15 07:34:41 +0700

    add annotation InstallIn on each module

--- Dependency Injection/TourismAppDaggerHilt/app/src/main/java/com/dicoding/tourismappdaggerhilt/core/di/RepositoryModule.kt
+++ Dependency Injection/TourismAppDaggerHilt/app/src/main/java/com/dicoding/tourismappdaggerhilt/core/di/RepositoryModule.kt
@@ -4,8 +4,11 @@ import com.dicoding.tourismappdaggerhilt.core.data.TourismRepository
 import com.dicoding.tourismappdaggerhilt.core.domain.repository.ITourismRepository
 import dagger.Binds
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
 @Module(includes = [NetworkModule::class, DatabaseModule::class])
+@InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
 
     @Binds
