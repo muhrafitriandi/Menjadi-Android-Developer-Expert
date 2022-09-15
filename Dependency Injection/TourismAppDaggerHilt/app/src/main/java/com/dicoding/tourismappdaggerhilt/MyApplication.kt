9de0034ac37ffed92d1e15eae476d029b484381f
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-09-15 07:29:22 +0700

    add annotation HiltAndroidApp

--- Dependency Injection/TourismAppDaggerHilt/app/src/main/java/com/dicoding/tourismappdaggerhilt/MyApplication.kt
+++ Dependency Injection/TourismAppDaggerHilt/app/src/main/java/com/dicoding/tourismappdaggerhilt/MyApplication.kt
@@ -5,14 +5,8 @@ import com.dicoding.tourismappdaggerhilt.core.di.CoreComponent
 import com.dicoding.tourismappdaggerhilt.core.di.DaggerCoreComponent
 import com.dicoding.tourismappdaggerhilt.di.AppComponent
 import com.dicoding.tourismappdaggerhilt.di.DaggerAppComponent
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 open class MyApplication : Application() {
-
-    private val coreComponent: CoreComponent by lazy {
-        DaggerCoreComponent.factory().create(applicationContext)
-    }
-
-    val appComponent: AppComponent by lazy {
-        DaggerAppComponent.factory().create(coreComponent)
-    }
 }
