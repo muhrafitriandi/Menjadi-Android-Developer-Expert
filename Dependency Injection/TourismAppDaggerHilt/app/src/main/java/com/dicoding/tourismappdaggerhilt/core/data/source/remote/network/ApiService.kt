aa896967be9dc66b8c3df916952816f839f7663d
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-09-15 07:23:58 +0700

    delete project

--- Dependency Injection/TourismAppDaggerHilt/app/src/main/java/com/dicoding/tourismappdaggerhilt/core/data/source/remote/network/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.dicoding.tourismappdaggerhilt.core.data.source.remote.network
-
-import com.dicoding.tourismappdaggerhilt.core.data.source.remote.response.ListTourismResponse
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("list")
-    suspend fun getList(): ListTourismResponse
-}
