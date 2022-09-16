bed168aabd5b5d538975774b972ed93009c67bc1
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-09-16 18:56:00 +0700

    tourism app modularization

---
+++ Modularization/TourismAppModularization/app/src/main/java/com/dicoding/tourismappmodularization/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.dicoding.tourismappmodularization.di
+
+import com.dicoding.tourismapp.core.domain.usecase.TourismInteractor
+import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
+import com.dicoding.tourismappmodularization.detail.DetailTourismViewModel
+import com.dicoding.tourismappmodularization.favorite.FavoriteViewModel
+import com.dicoding.tourismappmodularization.home.HomeViewModel
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val useCaseModule = module {
+    factory<TourismUseCase> { TourismInteractor(get()) }
+}
+
+val viewModelModule = module {
+    viewModel { HomeViewModel(get()) }
+    viewModel { FavoriteViewModel(get()) }
+    viewModel { DetailTourismViewModel(get()) }
+}
