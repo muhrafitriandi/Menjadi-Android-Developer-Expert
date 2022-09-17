90891b78a10d867b419a95fc2582d95d8deaa2f6
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-09-17 17:54:32 +0700

    Implementing Smooth Bottom Nav

--- Capstone Proyek Aplikasi Android/GitHubApp/app/src/main/java/com/yandey/githubapp/MainActivity.kt
+++ Capstone Proyek Aplikasi Android/GitHubApp/app/src/main/java/com/yandey/githubapp/MainActivity.kt
@@ -3,16 +3,42 @@ package com.yandey.githubapp
 import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 import android.view.animation.AnticipateInterpolator
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.yandey.githubapp.databinding.ActivityMainBinding
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreenWithAnim(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.navHostFragment)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
+        binding.bottomBar.setupWithNavController(menu!!, navController)
+        return false
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return true
     }
 
     private fun installSplashScreenWithAnim(savedInstanceState: Bundle?) {
